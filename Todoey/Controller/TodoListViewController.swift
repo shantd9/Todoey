ad5eee02a32eda61b9348241391f447e297e7e8c
Shant <shantd9@hotmail.com>
2019-12-22 18:35:55 +0200

    Using Codable to save data

---
+++ Todoey/Controller/TodoListViewController.swift
@@ -0,0 +1,88 @@
+//
+//  TodoListViewController.swift
+//  Todoey
+//
+//  Created by Shant Derounian on 12/21/19.
+//  Copyright © 2019 Shant Derounian. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    var itemArray = [Item]()
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("items.plist")
+    let defaults = UserDefaults.standard
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        loadItems()
+    }
+
+    //MARK: TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        cell.accessoryType = item.done ? .checkmark : .none
+        return cell
+    }
+    
+    //MARK: TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        self.saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+
+    }
+    
+    //MARK: Add new items
+    @IBAction func addButtonPressed(_ sender: Any) {
+        let alert =  UIAlertController(title: "Add new Todoey item", message: "", preferredStyle: .alert)
+        var textField = UITextField()
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            self.saveItems()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
+    //MARK: Private methods
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding value \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+            }
+            catch {
+                print("Error loading data")
+            }
+            
+        }
+    }
+}
+
---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Shant Derounian on 12/22/19.
+//  Copyright © 2019 Shant Derounian. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable {
+    var title: String = ""
+    var done: Bool = false
+    
+}
